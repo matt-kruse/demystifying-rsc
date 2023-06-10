d923ec60c31dacc47b4630cc6143698035067c48
Kruse Matt <github@mattkruse.com>
2023-06-10 09:14:23 -0500

    update

--- app/static-content/2/page.js
+++ app/static-content/2/page.js
@@ -3,16 +3,20 @@ import ObserverWindow, {filterRawEvents, filterVirtualDom} from "@/components/ob
 export default ()=><>
   <h2>Virtual DOM</h2>
 
-  <p>If you <a href="/view-source/static-content/2/" className="view-source" target="_blank">View Source</a> of this page, you'll see the javascript at the bottom contains this:</p>
+  <p>If you <a href="/view-source/static-content/2/" className="view-source" target="_blank">View Source</a> of this page, you'll see the javascript at the bottom contains script tags that contain an encoded form of this content:</p>
   <ObserverWindow inline={true} filter={filterRawEvents}/>
   <p>This is React's new line-based internal data streaming format.</p>
+  <p>The content is pushed into an array to be processed and split by newlines, which results in the content above, which is the raw React streaming data format.</p>
+  <h3>What Is It?</h3>
+  <p>It's a compact string representation of the virtual DOM, with abbreviations, internal references, and characters with encoded special meanings.</p>
   <h3>Why is it needed?</h3>
   <ul>
     <li>It contains a Virtual DOM representation of the static page that was generated. <i>(See the box below for an expanded view of the Virtual DOM of the generated page)</i></li>
     <li>If there are client (browser) React components, it needs to know where to insert them at run-time</li>
     <li>When routing happens, only the modified parts of the page will be updated (we'll see this later), so it needs to have a virtual representation of the page to dynamically update the DOM</li>
   </ul>
-
+  <h3>Current Mental Model</h3>
+  <p><img src={"/mental-model-2.png"}/></p>
   <h3>A few notes about this format</h3>
     <ul>
       <li>Lines are separated with a \n, so this is a line-based format, not JSON, for example</li>
@@ -20,7 +24,7 @@ export default ()=><>
       <li>Each line is in the format "ID:TYPE?JSON"</li>
       <li>This data format is not documented anywhere!</li>
       <li>The exact format has been seen to change between React canary releases, so it's a moving target.</li>
-      <li>Lines can contain references to other lines by using $ID in their content</li>
+      <li>Lines can contain references to other lines by using $ID or $LID (ex: $2 or $L2) in their content</li>
       <li>This format is considered "internal" to React and is not meant for human consumption. You do not need to know how it works to make apps with RSC. But it's useful to see what is happening behind the scenes.</li>
       <li>The <a href="https://rsc-parser.vercel.app/" target="_blank">RSC Parser</a> is a tool built by Alvar Lagerlöf that explores this format.</li>
     </ul>
@@ -32,7 +36,7 @@ export default ()=><>
 
   <h3>Virtual DOM Reconciliation</h3>
   <p>When the page loads, React does a reconciliation between the Virtual DOM that it thinks represents the page, and the actual static DOM that the server returned. If any differences are found, it throws a console error. This is because it won't be able to accurately update the DOM if it doesn't have the correct structure representation.</p>
-
+  <p><img src={"/mental-model-3.png"}/></p>
   <p>
     <a className={"button"} href={"/client-components/"}>Integrating Client Components</a>
   </p>
--- app/static-content/page.js
+++ app/static-content/page.js
@@ -4,6 +4,8 @@ export default async()=>{
     <p>If you <a className="view-source" href="/view-source/static-content/?highlight=What is all that javascript at the bottom of the page||self.__next_f.push" target="_blank">View Source</a> in your browser you'll see all this content as html in the source.</p>
     <p>In a typical SPA, the HTML delivered by the server would be an empty page, along with lots of javascript. That javascript would build the html and insert it into the page. Your html would appear in the DOM Inspector, but not in View Source.</p>
     <p>But with RSC, React components are run on the server and their HTML output is sent directly as the source.</p>
+    <h3>Current Mental Model</h3>
+    <p><img src={"/mental-model-1.png"}/></p>
     <p><b>But wait... </b></p>
     <p>What is all that javascript at the bottom of the page?</p>
     <p>Why is so much javascript included when RSC generates html, and there are no javascript components on this page?</p>
