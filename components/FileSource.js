1fb774c9628c8aeec7ebecf908a24b3607e3a9a3
Kruse Matt <github@mattkruse.com>
2023-06-17 06:50:37 -0500

    debug

--- components/FileSource.js
+++ components/FileSource.js
@@ -3,7 +3,8 @@ import path from 'path';
 
 export default function({filepath,title}) {
   try {
-    const source = fs.readFileSync(path.join(process.cwd(), filepath), 'utf-8');
+    // const source = fs.readFileSync(path.join(process.cwd(), filepath), 'utf-8');
+    const source = fs.readFileSync(filepath, 'utf-8');
     return <>
       {title ? <span className={"file-source-title"}>{title}</span> : ''}
       <pre className={"code"}>{source}</pre>
