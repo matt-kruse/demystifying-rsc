baf8831ba4b73343441a4e0f2be73f0eebf3a423
Kruse Matt <github@mattkruse.com>
2023-06-16 20:38:55 -0500

    file error trapping

--- components/FileSource.js
+++ components/FileSource.js
@@ -2,9 +2,16 @@ import fs from 'fs';
 import path from 'path';
 
 export default function({filepath,title}) {
-  const source = fs.readFileSync(path.join(process.cwd(),filepath),'utf-8');
-  return <>
-    {title?<span className={"file-source-title"}>{title}</span>:''}
-    <pre className={"code"}>{source}</pre>
-  </>
+  try {
+    const source = fs.readFileSync(path.join(process.cwd(), filepath), 'utf-8');
+    return <>
+      {title ? <span className={"file-source-title"}>{title}</span> : ''}
+      <pre className={"code"}>{source}</pre>
+    </>
+  } catch(e) {
+    return <pre className={"code"}>Error loading {path.join(process.cwd(), filepath}
+
+{e}
+    </pre>
+  }
 }
