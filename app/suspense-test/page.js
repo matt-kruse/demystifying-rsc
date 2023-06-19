a6b22b8049e9213263046e18d19646ba5c35e60c
Kruse Matt <github@mattkruse.com>
2023-06-19 06:44:55 -0500

    suspense-test

--- app/suspense-test/page.js
+++ app/suspense-test/page.js
@@ -1,13 +1,11 @@
 import {Suspense} from "react";
+export const dynamic='force-dynamic';
 
-function Delay() {
-  return new Promise((resolve)=>{
-    setTimeout(()=>resolve(
-        <div className={"box"}>Delayed Content: {Date.now()}</div>
-    ),5000);
+async function Delay() {
+  return new Promise(r=>{
+    setTimeout(()=>r(<div>Delayed Content: {Date.now()}</div>),5000);
   });
 }
-
 export default async()=>{
   return <Suspense fallback={<div>Loading...</div>}><Delay/></Suspense>
 }
