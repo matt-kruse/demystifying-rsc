b2cc4fc6020a6d8deafee83779a5ec9c3a895cd7
Alexander Mandrov <45426001+Unsleeping@users.noreply.github.com>
2024-11-24 11:11:06 +0200

    Update page.js

--- app/app-router/(route-groups)/route-groups/page.js
+++ app/app-router/(route-groups)/route-groups/page.js
@@ -7,7 +7,7 @@ export default ()=>{
         <PageInfo filename="/app-router/(route-groups)/route-groups/page.js"/>
         <h3>Route Groups</h3>
         <p>Note the path of the page.js file that contains this content. The (route-groups) directory is not included in the path. Any directory in (parentheses) is ignored, and its subdirectories are "pushed up" a level.</p>
-        <p>That's why this page can be accessed at /app-router/app-groups/ instead of /app-router/(route-groups)/route-groups/</p>
+        <p>That's why this page can be accessed at /app-router/route-groups/ instead of /app-router/(route-groups)/route-groups/</p>
         <ul>
           <li><Link href={"../route-group-1/"}>route-group-1</Link></li>
           <li><Link href={"../route-group-2/"}>route-group-2</Link></li>
